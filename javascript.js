bef185b2f380b06842032a924c1ab2786e910d1a
CaptureB <airdavid62@gmail.com>
2023-01-23 18:50:46 -0700

    Started html layout added basic styling js

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const key = config.APIKey;
+const searchResults = dcoument.getElementById('searchResults');
+const searchBtn = document.getElementById('searchBtn');
+const searchHistory = document.getElementById('weatherContent');
+const fiveDay = document.getElementById('fiveDay');
+const cityDetail = document.getElementById('cityDetail');
+
+
+
+function getApi() {
+    let requestUrl = ""
+}
